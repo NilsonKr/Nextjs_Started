3e531e59bc425ee60e9cf4c66a0a462adb3a91e5
NilsonKr <nuotrok444@gmail.com>
2021-07-17 15:54:22 -0500

    :sparkles: [ADD] Endpoint for product details

--- pages/api/avo/[id].ts
+++ pages/api/avo/[id].ts
@@ -1,12 +1,13 @@
-import DB from '@database';
 import { NextApiRequest, NextApiResponse } from 'next';
+import DB from '@database';
 
 const db = new DB();
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-	const allItems = await db.getAll();
+	const productId = req.query.id;
+	const item = await db.getById(productId as string);
 
-	res.status(200).json({ length: allItems.length, data: allItems });
+	res.status(200).json(item);
 }
 
 export default handler;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,5 @@
+import * as React from 'react';
+
 const Home = () => {
 	return (
 		<div>
